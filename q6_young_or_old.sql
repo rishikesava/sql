6ebe39c8624be92bce8fa7d456efbe301ea25ac5
rishikesava <89317326+rishikesava@users.noreply.github.com>
2021-12-23 12:26:42 +0530

    Delete q6_young_or_old.sql

--- q6_young_or_old.sql
+++
@@ -1,9 +0,0 @@
-/* 
-answer is false , younger earns more(except age 50),why order by invalid in view?
-*/
-create view [analysis2] as 
-select distinct age,max(Rate) as max_rate,PayFrequency,NationalIDNumber from HumanResources.Employee e1 inner join 
-HumanResources.EmployeePayHistory e2 on e1.BusinessEntityID = e2.BusinessEntityID
-where OrganizationLevel = 1 group by age,NationalIDNumber,Rate,PayFrequency
-
-select * from [analysis2]
