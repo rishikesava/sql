cbcfe50564d50daef97a70a13210b7ea04a91114
rishikesava <89317326+rishikesava@users.noreply.github.com>
2021-12-23 12:26:14 +0530

    Delete sum_total_high_rank_q4.sql

--- sum_total_high_rank_q4.sql
+++
@@ -1,8 +0,0 @@
-/*
- Sum total of all earnings of employees holding highest title 
-across all departments
-*/
-select DepartmentID,sum (Rate) as total_sum,sum(PayFrequency) as total_freq,sum (Rate) *sum(PayFrequency) as total
-from HumanResources.Employee e1 inner join HumanResources.EmployeePayHistory e2
-on e1.BusinessEntityID = e2.BusinessEntityID inner join HumanResources.EmployeeDepartmentHistory e3 on e2.BusinessEntityID = e3.BusinessEntityID
- where OrganizationLevel = 1 group by DepartmentID order by total desc
