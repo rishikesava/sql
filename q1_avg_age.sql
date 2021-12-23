ef99cb6e17b2d9e8d450e5ca4ff1bc9bee57673c
rishikesava <89317326+rishikesava@users.noreply.github.com>
2021-12-23 12:26:51 +0530

    Delete q1_avg_age.sql

--- q1_avg_age.sql
+++
@@ -1,5 +0,0 @@
-/* first created a column age in employee table and by default the values are null.later updated the values using 
-function datediff(YY,BirthDate,getdate()) as age.now a column with desired values as been created and performed avg function on 
-it to get avg age which is 42*/
-
-select avg (age) as avg_age from HumanResources.Employee;
